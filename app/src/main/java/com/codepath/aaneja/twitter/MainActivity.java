8997782968454a7f0940cc1ddbfe33f0bdce61f5
aaneja <anant.aneja@gmail.com>
2017-03-31 15:40:13 -0700

    - Basic tab views hooked up correctly

--- app/src/main/java/com/codepath/aaneja/twitter/MainActivity.java
+++ app/src/main/java/com/codepath/aaneja/twitter/MainActivity.java
@@ -2,11 +2,17 @@ package com.codepath.aaneja.twitter;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.codepath.aaneja.twitter.fragments.TimelineFragment;
 import com.codepath.aaneja.twitter.network.TwitterRestClient;
@@ -28,9 +34,35 @@ public class MainActivity extends AppCompatActivity implements TimelineFragment.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        // Get the ViewPager and set it's PagerAdapter so that it can display items
+        ViewPager viewPager = (ViewPager) findViewById(R.id.vpPager);
+        viewPager.setAdapter(new ViewPagerHomeAdapter(getSupportFragmentManager()));
+
+        // Give the TabLayout the ViewPager
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+        /*FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.phTimeline, TimelineFragment.newInstance(TwitterRestClient.API.USER_TIMELINE,"YawnOkPlease"));
-        ft.commit();
+        ft.commit();*/
+        //viewPager.setCurrentItem(0);
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                Toast.makeText(MainActivity.this, "Selected tab with Name: " + tab.getText(), Toast.LENGTH_SHORT).show();
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
     public void onComposeAction(MenuItem item) {
@@ -62,5 +94,45 @@ public class MainActivity extends AppCompatActivity implements TimelineFragment.
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    public static class ViewPagerHomeAdapter extends FragmentPagerAdapter {
+
+        private static int NUM_ITEMS = 2;
+        private final FragmentManager fragmentManager;
+
+        public ViewPagerHomeAdapter(FragmentManager fm) {
+            super(fm);
+            fragmentManager = fm;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                case 0:
+                    return TimelineFragment.newInstance(TwitterRestClient.API.HOME_TIMELINE,"");
+                case 1:
+                    return TimelineFragment.newInstance(TwitterRestClient.API.MENTIONS,"");
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return NUM_ITEMS;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+            case 0: // Fragment # 0 - This will show FirstFragment
+            return "HOME";
+            case 1: // Fragment # 0 - This will show FirstFragment different title
+            return "MENTIONS";
+            default:
+            return null;
+            }
+        }
     }
 }
